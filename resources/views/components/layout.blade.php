976b4fcb062c7e36dcb66c7b8666c96620f4ae0b
CountryMartin <Martin.linders@home.nl>
2024-01-21 22:06:42 +0100

    Mapjes voor styling aangemaakt

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,29 +10,29 @@
 
 </head>
 <body class="flex flex-col min-h-screen">
-    <nav>
+    {{-- <nav>
         <div name="menubar" class="bg-purple-700 w-full text-xs lg:text-xl h-14 shadow-sm pl-1 border-b-4 border-b-green-400">
-            <ul class='pt-2 pb-1 flex items-center'>
+            <ul class='pt-2 pb-1 flex items-center'> --}}
                 {{-- Welcome --}}
-                @if(Auth::check())
-                <li class="text-white float-left mr-3 pr-3 border border-solid border-transparent flex flex-row">Welkom&NonBreakingSpace;<div class="text-green-400 font-bold"><a href="{{route('profileView')}}" class="hover:underline hover:text-green-300">{{Auth::user()->first_name}}</a></div></li>
-                @endif 
+                {{-- @if(Auth::check())
+                <li class="">Welkom&NonBreakingSpace;<div class=""><a href="{{route('profileView')}}" class="hover:underline hover:text-green-300">{{Auth::user()->first_name}}</a></div></li>
+                @endif  --}}
                 {{-- home --}}
-                <li class="text-white float-left block mr-3 pr-3 border border-solid border-transparent hover:bg-purple-500 hover:border hover:border-solid rounded-md transition duration-150"><a href="{{route('index')}}">home</a></li>
+                {{-- <li class=""><a href="{{route('index')}}">home</a></li> --}}
                 {{-- login / signup --}}
-                @if(!Auth::check())
-                    <li class="text-white float-left block mr-3 pr-3 border border-solid border-transparent hover:bg-purple-500 hover:border hover:border-solid rounded-md transition duration-150"><a href="{{route('login')}}">login</a></li>
-                    <li class="text-white float-left block mr-3 pr-3 border border-solid border-transparent hover:bg-purple-500 hover:border hover:border-solid rounded-md transition duration-150"><a href="{{route('signup')}}">registreren</a></li>
-                @endif
+                {{-- @if(!Auth::check())
+                    <li class=""><a href="{{route('login')}}">login</a></li>
+                    <li class=""><a href="{{route('signup')}}">registreren</a></li>
+                @endif --}}
                 {{-- accomodation --}}
-                <li class="text-white float-left block mr-3 pr-3 border border-solid border-transparent hover:bg-purple-500 hover:border hover:border-solid rounded-md transition duration-150"><a href="{{route('showacco')}}">mijn Paarspop weekend</a></li>
+                {{-- <li class=""><a href="{{route('showacco')}}">mijn Paarspop weekend</a></li>
                 @if(Auth::check())
-                <li class="text-white float-right block mr-3 pr-3 border border-solid border-transparent hover:bg-purple-500 hover:border hover:border-solid rounded-md transition duration-150"><a href="{{route('logout')}}">log uit</a></li>
+                <li class=""><a href="{{route('logout')}}">log uit</a></li>
                 @endif
                 @if(Auth::check() && Auth::user()->admin==1)
-                <li class="text-white float-right block mr-3 pr-3 border border-solid border-transparent hover:bg-purple-500 hover:border hover:border-solid rounded-md transition duration-150"><a href="{{route('adminIndex')}}">Admin</a></li>
-                @endif
-            </ul>
+                <li class=""><a href="{{route('adminIndex')}}">Admin</a></li>
+                @endif --}}
+            {{-- </ul>
         </div>
     </nav>
     <article class="flex-grow">
@@ -59,8 +59,8 @@
                     <p><a href="tel:+310654221833p000" class="hover:underline">06-54221833</a></p>
                 </div>
             </div>
-        </div>
+        </div> --}}
         
-    </footer>
+    {{-- </footer> --}}
 </body>
 </html>
