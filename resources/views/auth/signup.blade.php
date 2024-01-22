b3f7387a6b71d814885821e4bbf78ba0c12e6844
Aaron Luijten <aluijten1011@gmail.com>
2024-01-22 12:02:34 +0100

    fixed merge conflicts (maybe). re-added some code lost in the merge

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -9,57 +9,57 @@
 
                 <div class="flex-grid-signup">
                 <!-- First Name -->
-                <div class="item-half-signup">
+                    <div class="item-half-signup">
                         <div class="input-box-signup">
                             <label for="first_name" class="flex"><p class="star">*&nbsp;</p>Voornaam:</label>
-                <input type="text" name="first_name" id="first_name" value="{{old('first_name')}}" class="input-text">
+                            <input type="text" name="first_name" id="first_name" value="{{old('first_name')}}" class="input-text">
                         </div>
-                </div>
-                <!-- Last Name -->
-                <div class="item-half-signup">
+                    </div>
+                    <!-- Last Name -->
+                    <div class="item-half-signup">
                         <div class="input-box-signup">
                             <label for="last_name" class="flex"><p class="star">*&nbsp;</p>Achternaam </label>
-                <input type="text" name="last_name" id="last_name" value="{{old('last_name')}}" class="input-text">
+                            <input type="text" name="last_name" id="last_name" value="{{old('last_name')}}" class="input-text">
                         </div>
-                </div>
-                
-                <!-- email -->
-                <div class="item-full-signup">
+                    </div>
+                    
+                    <!-- email -->
+                    <div class="item-full-signup">
                         <div class="input-box-signup">
                             <label for="email" class="flex"><p class="star">*&nbsp;</p>Email:</label>
-                <input type="text" name="email" id="email" value="{{old('email')}}" class="input-text">
-                </div>
-                     </div>
-
-                <!-- phonenumber -->
-                <div class="item-half-signup">
-                        <div class="input-box-signup">
-                            <label for="phonenumber" class="flex"><p class="star">*&nbsp;</p>Telefoon:</label>
-                <input type="text" name="phonenumber" id="phonenumber" value="{{old('phonenumber')}}" class="input-text">
+                            <input type="text" name="email" id="email" value="{{old('email')}}" class="input-text">
                         </div>
-                </div>
-
-                <!-- date_of_birth -->
-                <div class="item-half-signup">
-                        <div class="input-box-signup">
-                            <label for="date_of_birth" class="flex"><p class="star">*&nbsp;</p> Geboorte datum:</label>
-                <input type="date" name="date_of_birth" id="date_of_birth" value="{{old('date_of_birth')}}" class="input-text">
-                        </div>
-                </div>
-
-                <!-- password  -->
-                <div class="item-half-signup">
-                        <div class="input-box-signup">
-                            <label for="password" class="flex"><p class="star">*&nbsp;</p>Wachtwoord:</label>
-                <input type="password" name="password" id="password" class="input-text">
-                </div>
                     </div>
 
-                <!-- password_confirm  -->
-                <div class="item-half-signup">
+                    <!-- phonenumber -->
+                    <div class="item-half-signup">
+                        <div class="input-box-signup">
+                            <label for="phonenumber" class="flex"><p class="star">*&nbsp;</p>Telefoon:</label>
+                            <input type="text" name="phonenumber" id="phonenumber" value="{{old('phonenumber')}}" class="input-text">
+                        </div>
+                    </div>
+
+                    <!-- date_of_birth -->
+                    <div class="item-half-signup">
+                        <div class="input-box-signup">
+                            <label for="date_of_birth" class="flex"><p class="star">*&nbsp;</p> Geboorte datum:</label>
+                            <input type="date" name="date_of_birth" id="date_of_birth" value="{{old('date_of_birth')}}" class="input-text">
+                        </div>
+                    </div>
+
+                    <!-- password  -->
+                    <div class="item-half-signup">
+                        <div class="input-box-signup">
+                            <label for="password" class="flex"><p class="star">*&nbsp;</p>Wachtwoord:</label>
+                            <input type="password" name="password" id="password" class="input-text">
+                        </div>
+                    </div>
+
+                    <!-- password_confirm  -->
+                    <div class="item-half-signup">
                         <div class="input-box-signup">
                             <label for="password_confirm" class="flex"><p class="star">*&nbsp;  </p> Herhaal wachtwoord: </label>
-                <input type="password" name="password_confirm" id="password_confirm" class="input-text">
+                            <input type="password" name="password_confirm" id="password_confirm" class="input-text">
                         </div>
                     </div>
                 </div>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -31,22 +31,22 @@
     </div>
 </nav>
 <body>
-    {{-- <nav>
-        <div name="menubar" class="bg-purple-700 w-full text-xs lg:text-xl h-14 shadow-sm pl-1 border-b-4 border-b-green-400">
-            <ul class='pt-2 pb-1 flex items-center'>
+    <nav>
+        <div name="menubar" class="">
+            <ul class=''>
                 {{-- Welcome --}}
-                {{-- @if(Auth::check())
-                <li class="">Welkom&NonBreakingSpace;<div class=""><a href="{{route('profileView')}}" class="hover:underline hover:text-green-300">{{Auth::user()->first_name}}</a></div></li>
-                @endif  --}}
+                @if(Auth::check())
+                <li class="">Welkom&NonBreakingSpace;<div class=""><a href="{{route('profileView')}}" class="">{{Auth::user()->first_name}}</a></div></li>
+                @endif 
                 {{-- home --}}
-                {{-- <li class=""><a href="{{route('index')}}">home</a></li> --}}
+                <li class=""><a href="{{route('index')}}">home</a></li>
                 {{-- login / signup --}}
-                {{-- @if(!Auth::check())
+                @if(!Auth::check())
                     <li class=""><a href="{{route('login')}}">login</a></li>
                     <li class=""><a href="{{route('signup')}}">registreren</a></li>
-                @endif --}}
+                @endif
                 {{-- accomodation --}}
-                {{-- <li class=""><a href="{{route('showacco')}}">mijn Paarspop weekend</a></li>
+                 <li class=""><a href="{{route('showacco')}}">mijn Paarspop weekend</a></li>
                 @if(Auth::check())
                 <li class=""><a href="{{route('logout')}}">log uit</a></li>
                 @endif
@@ -56,28 +56,28 @@
              </ul>
         </div>
     </nav>
-    <article class="flex-grow">
+    <article class="">
         <div>
             {{$slot}}
         </div>
-    </article> --}}
+    </article>
 
-    <footer class="bg-purple-700 w-full text-sm lg:text-xl border-t-4 border-t-green-400 h-fit">
-        <div class=" h-12 flex items-center justify-center text-green-400">
-            <p>Made by <a href="mailto: aluijten1011@gmail.com" class="hover:underline">Aaron Luijten</a></p>
+    <footer class="">
+        <div class=" ">
+            <p>Made by <a href="mailto: aluijten1011@gmail.com" class="">Aaron Luijten</a></p>
         </div>
-        <div class="ml-3">
-            <h2 class="text-green-400 font-bold">Contact</h2>
-            <div class="flex justify-start flex-row w-2/6">
+        <div class="">
+            <h2 class="">Contact</h2>
+            <div class="">
                 
-                <div class="text-white">
+                <div class="">
                     <p>email:</p>
                     <p>Telefoon: </p>
                 </div>
                 
-                <div class="flex items-baseline flex-col ml-5 text-green-400 ">
-                    <p><a href="mailto: paarspop23@gmail.com" class="hover:underline">paarspop23@gmail.com</a></p>
-                    <p><a href="tel:+310654221833p000" class="hover:underline">06-54221833</a></p>
+                <div class="">
+                    <p><a href="mailto: paarspop23@gmail.com" class="">paarspop23@gmail.com</a></p>
+                    <p><a href="tel:+310654221833p000" class="">06-54221833</a></p>
                 </div>
             </div>
         </div>
