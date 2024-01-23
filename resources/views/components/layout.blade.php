0b605cea524e249e3feb834acfb34570f4ff95dc
CountryMartin <Martin.linders@home.nl>
2024-01-23 03:23:55 +0100

    menu foutje gefixt

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -30,10 +30,10 @@
         <div class="row2">
             <a href="{{route('index')}}" class="{{ Request::is('/') ? 'active' : '' }} menu-link">Home</a>
             <a href="{{route('index')}}" class="menu-link">Over ons</a>
-            <a href="{{route('contact')}}" class="menu-link">Contact</a>
+            <a href="{{route('contact')}}" class="{{ Request::is('contact') ? 'active' : '' }} menu-link">Contact</a>
             @if (Auth::check())
                 @if (Auth::user()->admin == 1)
-                    <a href="{{route('adminIndex')}}" class="{{ Request::is('/admin') ? 'active' : '' }} menu-link">Admin</a>
+                    <a href="{{route('adminIndex')}}" class="{{ Request::is('admin') ? 'active' : '' }} menu-link">Admin</a>
                 @endif
             @endif
         </div>
