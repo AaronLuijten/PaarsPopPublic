4e81373b5049485f96125bc1cfb732f09c047bad
CountryMartin <Martin.linders@home.nl>
2024-03-21 00:29:51 +0100

    Mobile homepage en menu

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -46,6 +46,43 @@
             
         </div>
     </div>
+    <!-- Load an icon library to show a hamburger menu (bars) on small screens -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Top Navigation Menu -->
+    {{-- <div class="topnav">
+        <a href="#home" class="active">Logo</a>
+        <!-- Navigation links (hidden by default) -->
+        <div id="myLinks">
+            <a href="news">News</a>
+            <a href="contact">Contact</a>
+        </div>
+        <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
+        <div class="row3">
+            @if (!Auth::check())
+                <button class="btn_outline"><a href="{{route('login')}}">Inloggen</a></button>
+                <button class="btn_filled ml-5"><a href="{{route('signup')}}">Registreren</a></button>
+            @else
+                <button class="btn_outline"><a href="{{route('logout')}}">uitloggen</a></button>
+            @endif
+            
+        </div>
+    </div> --}}
+
+    <script>
+        /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+        function myFunction() {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+        }
+    </script>
 </nav>
 <body class="mainBody">
     <div id="bodyContainer">
