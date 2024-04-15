e309f5f53640ed3c8350fafa80ae8b2759cb4af4
Aaron Luijten <aluijten1011@gmail.com>
2024-04-15 23:01:52 +0200

    Added javascript to enlarge picture on click

--- public/js/app.js
+++ public/js/app.js
@@ -2058,9 +2058,17 @@ module.exports = {
 /*!*****************************!*\
   !*** ./resources/js/app.js ***!
   \*****************************/
-/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _bootstrap__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
+/* harmony import */ var _bootstrap__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_bootstrap__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _scripts__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./scripts */ "./resources/js/scripts.js");
+/* harmony import */ var _scripts__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_scripts__WEBPACK_IMPORTED_MODULE_1__);
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
+
+
 
 /***/ }),
 
@@ -2097,6 +2105,37 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+/***/ }),
+
+/***/ "./resources/js/scripts.js":
+/*!*********************************!*\
+  !*** ./resources/js/scripts.js ***!
+  \*********************************/
+/***/ (() => {
+
+// const { boolean } = require("yargs");
+
+// window.onload = function () {
+
+//     function switchEnlarge()
+//     {
+//         let img = document.getElementById("parkeren");
+//         let b = true;
+
+//         if(b)
+//         {
+//             img.style.transform = "scale(1.5)";
+//             img.style.transition = "transition 0.25s ease";
+//         }
+//         else
+//         {
+//             img.style.transform = "scale(1.0)";
+//             img.style.transition = "transition 0.25s ease";
+//         }
+
+//     }
+// }
 
 /***/ }),
 
@@ -19592,6 +19631,30 @@ module.exports = JSON.parse('{"name":"axios","version":"0.21.4","description":"P
 /******/ 				}
 /******/ 			}
 /******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
 /******/ 		};
 /******/ 	})();
 /******/ 	
--- resources/views/home/festivalMap.blade.php
+++ resources/views/home/festivalMap.blade.php
@@ -1,21 +1,52 @@
 <x-layout>
-    <div class="flex flex-col items-center">
+    <div class="flex-information">
         <h2>Parkeer informatie</h2>
-        <div>
+        <div class=flex-parking>
             <div>
-                <img src="{{asset('assets/images/Parkeer overzicht.jpg')}}" alt="Foto van parkeer overzicht">
+                <img id="parkeren" src="{{asset('assets/images/Parkeer overzicht.jpg')}}" alt="Foto van parkeer overzicht" onclick="switchEnlarge()">
+            </div>
+            <div class="parking-text">
+                <p>
+                    Ons festivalterrein heeft helaas geen eigen parkeerterrein. De mogelijkheden om in onze straat te parkeren zijn beperkt en we willen ook de overlast met betrekking tot parkeren voor onze buren minimaliseren.  Laden en Lossen bij ons voor; géén probleem, maar we zouden het fijn vinden als je daarna je auto ergens anders parkeert.
+                    Er zijn op de volgende plekken openbare parkeerplaatsen
+                    -    Het Weike van Bus (achter de flat)
+                    -    Bij het Dorpshuis in de havenstraat en aan de haven
+                    -    Parkeerplaats bij voetbal en tennis 
+                    Nb.. Je hoeft in Stampersgat nergens te betalen om te parkeren 😉</p>
             </div>
         </div>
         <h2>Festival plattegrond</h2>
-        <div class="bg-purple-500 p-2 rounded-md flex flex-col items-center w-11/12 lg:w-7/12 m-2">
-            <div class="mb-3 bg-purple-400 p-1 rounded-md">
-                <img src="{{asset('assets/images/Festival terein 3D vanaf achterzijde huis.jpg')}}" width="" alt="Foto Festival 3D achterzijde huis" class="border-2 border-green-400 rounded-md">
+        <div class="flex-map">
+            <div class="">
+                <img src="{{asset('assets/images/Festival terein 3D vanaf achterzijde huis.jpg')}}" width="" alt="Foto Festival 3D achterzijde huis" class="">
                 <p>Festival terein 3D</p>
             </div>
-            <div class="mb-3 bg-purple-400 p-1 rounded-md">
-                <img src="{{asset('assets/images/Festival terein 3D vanaf achterzijd tuin.jpg')}}" width="" alt="Foto Festival 3D achterzijde huis" class="border-2 border-green-400 rounded-md">
+            <div class="">
+                <img src="{{asset('assets/images/Festival terein 3D vanaf achterzijd tuin.jpg')}}" width="" alt="Foto Festival 3D achterzijde huis" class="">
                 <p>Festival terein 3D</p>
             </div>
         </div>
     </div>
 </x-layout>
+<script>
+var b = true;
+function switchEnlarge()
+    {
+        let img = document.getElementById("parkeren");
+
+        if(b == true)
+        {
+            img.style.transform = "scale(1.4)";
+            img.style.transition = "transition 0.25s ease";
+            b = false;
+        }
+        else
+        {
+            img.style.transform = "scale(1.0)";
+            img.style.transition = "transition 0.25s ease";
+            b = true;
+        }
+        
+    }
+    
+</script>
